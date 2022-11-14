bfd343e069a79ad0668285e98109e0a7de30184b
Daniel Aviidow <aviidow@gmail.com>
2022-11-14 14:46:30 +0300

    jdbc fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+    // добавлен модификатор private
     private final UserDao dao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
